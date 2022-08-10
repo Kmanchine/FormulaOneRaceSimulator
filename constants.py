f6d186d598d657b406f803a2a52a9d88aedd71ab
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-08-10 00:08:18 -0400

    Added constants

--- constants.py
+++ constants.py
@@ -9,14 +9,23 @@ import matplotlib.patches as mpatches
 # Enable the cache by providing the name of the cache folder
 ff1.Cache.enable_cache('cache')
 
+# Constants for tyre strings
+SOFT = 'SOFT'
+MEDIUM = 'MEDIUM'
+HARD = 'HARD'
+INTERMEDIATE = 'INTERMEDIATE'
+WET = 'WET'
+
 # Set the mapping for plot legend labels
-SOFT = mpatches.Patch(color='red', label='Soft')
-MEDIUM = mpatches.Patch(color='yellow', label='Medium')
-HARD = mpatches.Patch(color='white', label='Hard')
-INTERMEDIATE = mpatches.Patch(color='green', label='Intermediate')
-WET = mpatches.Patch(color='blue', label='Wet')
-TYRES_COLOR_LEGEND = [SOFT, MEDIUM, HARD, INTERMEDIATE, WET]
+SOFT_PATCHES = mpatches.Patch(color='red', label='Soft')
+MEDIUM_PATCHES = mpatches.Patch(color='yellow', label='Medium')
+HARD_PATCHES = mpatches.Patch(color='white', label='Hard')
+INTERMEDIATE_PATCHES = mpatches.Patch(color='green', label='Intermediate')
+WET_PATCHES = mpatches.Patch(color='blue', label='Wet')
+TYRES_COLOR_LEGEND = [SOFT_PATCHES, MEDIUM_PATCHES, HARD_PATCHES, INTERMEDIATE_PATCHES, WET_PATCHES]
 
 # Another color mapping... just for the purpose of line plot
 TYRES_COLOR_DICT = {"SOFT": "red", "MEDIUM": "#FFC000", "HARD": "white", "INTERMEDIATE": "green",
                  "WET": "blue"}
+
+TYRE_DEGRADATION_REGRESSION_DEGREE = 2
