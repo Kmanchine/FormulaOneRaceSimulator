3852510cd4708af56199eedd219e7866d612db12
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-08-21 23:05:36 -0400

    Added teammate pair dict

--- constants.py
+++ constants.py
@@ -28,8 +28,13 @@ TYRES_COLOR_LEGEND = [SOFT_PATCHES, MEDIUM_PATCHES, HARD_PATCHES, INTERMEDIATE_P
 
 # Another color mapping... just for the purpose of line plot
 TYRES_COLOR_DICT = {"SOFT": "red", "MEDIUM": "#FFC000", "HARD": "white", "INTERMEDIATE": "green",
-                 "WET": "blue"}
+                    "WET": "blue"}
 
 TYRE_DEGRADATION_REGRESSION_DEGREE = 2
 
 QUALIFYING_SESSIONS = ['Q1', 'Q2', 'Q3']
+
+# Teammate pairs in 2022 and values can be keys
+TEAMMATE_PAIRS_DICT = {'RUS': 'HAM', 'VER': 'PER', 'MSC': 'MAG', 'BOT': 'ZHO', 'RIC': 'NOR',
+                       'STR': 'VET', 'ALB': 'LAT', 'TSU': 'GAS', 'OCO': 'ALO', 'SAI': 'LEC'}
+TEAMMATE_PAIRS_DICT.update({v: k for k, v in TEAMMATE_PAIRS_DICT.items()})
