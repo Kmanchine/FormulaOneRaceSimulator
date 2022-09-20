6773c8162341354701fec84283de5d32b42ac8d2
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-09-20 00:50:27 -0400

    Added basic user guide

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ Tutorial for fastf1 library
 ##
 
 import fastf1 as ff1
-##
 
 # Enable the cache by providing the name of the cache folder
 ff1.Cache.enable_cache('cache')
+
+# Check out the .ipynb and .py files for documentation and explanation.
